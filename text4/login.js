5c436315b57a4be00149c99610420410a89d3527
ivan59438 <121140890+ivan59438@users.noreply.github.com>
2022-12-21 09:38:54 +0800

    add

---
+++ text4/login.js
@@ -0,0 +1,15 @@
+var x =document.getElementById("login");
+var y =document.getElementById("register");
+var z =document.getElementById("btns");
+
+
+function register(){
+    x.style.left="550px"
+    y.style.left="0px"
+    z.style.left="150px"
+}
+function login(){
+    x.style.left="0px"
+    y.style.left="550px"
+    z.style.left="0px"
+}
